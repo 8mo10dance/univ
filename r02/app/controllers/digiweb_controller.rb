958dc0e82d2d9c84a5a114f430570abc5f8c0d35
dexmon <dxm.issatisfied@gmail.com>
2019-06-20 01:09:44 +0900

    feat: MenuListのAPIを作成する

--- r02/app/controllers/digiweb_controller.rb
+++ r02/app/controllers/digiweb_controller.rb
@@ -2,20 +2,6 @@
 
 class DigiwebController < ApplicationController
   def show
-    @list_menu_list = list_menu_list
     @histories = Digiweb::UpdateHistory.order(id: :DESC)
   end
-
-  private
-
-    def list_menu_list
-      Digiweb::MenuList.category_code.values.map do |category_code|
-        {
-          name: category_code.text,
-          items: Digiweb::MenuList.where(category_code: category_code).map do |menu_list|
-            { name: menu_list.name, url: menu_list.url }
-          end,
-        }
-      end
-    end
 end
--- r02/config/routes.rb
+++ r02/config/routes.rb
@@ -2,4 +2,10 @@
 
 Rails.application.routes.draw do
   resource 'digiweb', only: :show, controller: 'digiweb'
+
+  namespace :api do
+    namespace :v1 do
+      resources :list_menus, only: :index
+    end
+  end
 end
