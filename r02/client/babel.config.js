048cc56708f4bf0e923e2bdb9bfadfbd5fb6ded9
dexmon <dexmon@example.com>
2019-11-23 23:53:31 +0900

    chore: tscで型チェックを行う

--- r02/client/babel.config.js
+++ r02/client/babel.config.js
@@ -2,7 +2,13 @@ module.exports = api => {
   api.cache(true)
 
   const presets = [
-    ['@babel/preset-env'],
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
     '@babel/preset-react',
     '@babel/typescript',
   ]
