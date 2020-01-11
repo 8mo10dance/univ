46cb8926b99d1f8015b449fff093128a93f6d0d7
dexmon <dexmon@example.com>
2020-01-11 17:45:11 +0900

    gleemanageのルーティングを設定する

---
+++ r02/app/controllers/glee_manages_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class GleeManagesController < ApplicationController
+  def index
+    render html: 'hoge'
+  end
+end
--- r02/config/routes.rb
+++ r02/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   resource 'digiweb', only: :show, controller: 'digiweb'
+  resources :glee_manages, only: %i[index]
 
   namespace :api do
     namespace :v1 do
