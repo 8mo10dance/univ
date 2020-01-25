ee2e8b86c03f1a947b284885c57858d9fdee136e
dexmon <dexmon@example.com>
2020-01-25 17:02:35 +0900

    seed で Article を登録する

--- r02/db/seeds.rb
+++ r02/db/seeds.rb
@@ -94,3 +94,25 @@ menu_list_params.reverse_each do |param|
     Digiweb::MenuList.create!(name: item[:name], url: item[:url], category_code: category_code == 'Link' ? 0 : 1)
   end
 end
+
+Dir::entries(Rails.root.join('public', 'articles')).each do |filename|
+  next unless filename =~ /.json$/
+
+  json = JSON.parse(File.open(Rails.root.join('public', 'articles', filename), 'r').read)
+  year = 2014
+  month = filename.split('').take(2).join('').to_i
+  day = filename.split('').take(4).drop(2).join('').to_i
+
+  ArticleGroup.transaction do
+    article_group = ArticleGroup.new
+
+    json.each do |v|
+      article = article_group.articles.build(
+        title: v['title'],
+        published_at: Date.new(year, month, day),
+      )
+      article.article_items.build(v['list'].map { |item| { text: item } })
+    end
+    article_group.save!
+  end
+end
