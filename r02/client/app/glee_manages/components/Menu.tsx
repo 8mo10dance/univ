c9c4680d2fa021f07ad815fbdc70625e56cdd883
Tomoya Sunda <dxm.issatisfied@gmail.com>
2020-02-05 12:03:12 +0900

    SideMenuをMaterial-UIで実装する

--- r02/client/app/glee_manages/components/Menu.tsx
+++ r02/client/app/glee_manages/components/Menu.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import { Divider, Button } from '@material-ui/core'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import { ArticleGroup } from 'types/models'
 import MenuList from 'components/MenuList'
 
@@ -7,6 +9,18 @@ interface Props {
 }
 
 const MENU_LIST_SIZE = 5
+
+const drawerWidth = 230
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      width: drawerWidth,
+      backgroundColor: '#878787',
+    },
+    toolbar: theme.mixins.toolbar,
+  })
+)
 
 export default ({ articleGroups }: Props) => {
   const [isLatest, toggle] = React.useState(true)
@@ -18,17 +32,20 @@ export default ({ articleGroups }: Props) => {
   const mainList = articleGroups.slice(0, MENU_LIST_SIZE)
   const subList = articleGroups.slice(MENU_LIST_SIZE)
 
+  const classes = useStyles()
+
   return (
-    <div id="menu">
-      <center>
-        <h3>議事録一覧</h3>
-      </center>
+    <div className={classes.drawer}>
+      <div className={classes.toolbar} />
+      <Divider />
       <MenuList list={isLatest ? mainList : subList} />
-      <a id="listBtn" href="#" onClick={handleClick}>
+      <Button color="default" onClick={handleClick}>
         {isLatest ? 'これ以前の議事録' : '最新の議事録'}
-      </a>
-      <hr />
-      <a href="index_old.php">前期議事録</a>
+      </Button>
+      <Divider />
+      <Button color="default" component="a" href="index_old.php">
+        前期議事録
+      </Button>
     </div>
   )
 }
--- r02/client/app/glee_manages/components/MenuList.tsx
+++ r02/client/app/glee_manages/components/MenuList.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { List, ListSubheader, ListItem, ListItemText } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
 import { format } from 'date-fns'
 import { ArticleGroup } from 'types/models'
 
@@ -13,16 +15,28 @@ const formatDate = (date: string) => {
   return format(d, 'yyyy年MM月dd日')
 }
 
+const useStyles = makeStyles(
+  createStyles({
+    root: {
+      width: '100%',
+    },
+  })
+)
+
 export default ({ list }: Props) => {
+  const classes = useStyles()
+
   return (
-    <ul style={{ listStyle: 'none' }}>
-      {list.map((item, index) => (
-        <li key={index}>
-          <Link to={`/${item.id}`}>
-            {`${formatDate(item.publishedAt)}マネミ`}
-          </Link>
-        </li>
+    <List
+      component="nav"
+      subheader={<ListSubheader component="h3">議事録一覧</ListSubheader>}
+      className={classes.root}
+    >
+      {list.map(item => (
+        <ListItem key={item.id} component={Link} to={`/${item.id}`}>
+          <ListItemText primary={`${formatDate(item.publishedAt)}マネミ`} />
+        </ListItem>
       ))}
-    </ul>
+    </List>
   )
 }
