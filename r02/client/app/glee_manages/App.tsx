9d3a50ee618288d09fc0ab1fa61f08d3da3e5f97
Tomoya Sunda <dxm.issatisfied@gmail.com>
2020-02-05 11:42:39 +0900

    Themeを設定する

--- r02/client/app/glee_manages/App.tsx
+++ r02/client/app/glee_manages/App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
-import { CssBaseline } from '@material-ui/core'
+import { ThemeProvider, CssBaseline } from '@material-ui/core'
 import { useArticleGroups } from 'hooks/articleGroups'
 import TitleBox from 'components/TitleBox'
 import Menu from 'components/Menu'
 import ArticleGroupBox from 'components/ArticleGroupBox'
+import theme from './theme'
 
 export default () => {
   const { articleGroups } = useArticleGroups()
@@ -12,16 +13,18 @@ export default () => {
   return (
     <Router>
       <CssBaseline />
-      <TitleBox />
-      <Menu articleGroups={articleGroups} />
-      <Switch>
-        <Route exact path="/">
-          <ArticleGroupBox />
-        </Route>
-        <Route path="/:id">
-          <ArticleGroupBox />
-        </Route>
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <TitleBox />
+        <Menu articleGroups={articleGroups} />
+        <Switch>
+          <Route exact path="/">
+            <ArticleGroupBox />
+          </Route>
+          <Route path="/:id">
+            <ArticleGroupBox />
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </Router>
   )
 }
