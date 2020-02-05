f8e7f24f7cb07e2e241079a64215aa995fbe67b2
Tomoya Sunda <dxm.issatisfied@gmail.com>
2020-02-05 11:45:04 +0900

    TitleBoxをMaterial-UIで実装する

--- r02/client/app/glee_manages/components/TitleBox.tsx
+++ r02/client/app/glee_manages/components/TitleBox.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+
+const drawerWidth = 230
+
+const useStyles = makeStyles(
+  createStyles({
+    appBar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  })
+)
 
 export default () => {
+  const classes = useStyles()
+
   return (
-    <div id="titleBox">
-      <h1>マネミ議事録</h1>
-    </div>
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6">マネミ議事録</Typography>
+      </Toolbar>
+    </AppBar>
   )
 }
