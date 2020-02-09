860848293aeabfdc6cb70f6ca6ff6c98d0dc3fd4
dexmon <dexmon@example.com>
2020-02-09 20:44:37 +0900

    redux-saga を導入する

--- r02/client/app/digiweb/store.ts
+++ r02/client/app/digiweb/store.ts
@@ -1,3 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import rootReducer from 'modules'
+import rootSaga from 'sagas'
 
-export default createStore(() => {})
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(rootSaga)
+
+export default store
