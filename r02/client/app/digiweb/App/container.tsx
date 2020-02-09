61b703b84dc1fb383565f95bceeeedc8e2075322
dexmon <dexmon@example.com>
2020-02-09 21:48:57 +0900

    コンポーネントマウント時に UpdateHistory を取得する

--- r02/client/app/digiweb/App/container.tsx
+++ r02/client/app/digiweb/App/container.tsx
@@ -1,3 +1,14 @@
+import * as React from 'react'
+import { useDispatch } from 'react-redux'
+import * as actions from 'modules/updateHistory/actions'
 import Presenter from './presenter'
 
-export default Presenter
+export default () => {
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+    dispatch(actions.getAll.start())
+  }, [])
+
+  return <Presenter />
+}
