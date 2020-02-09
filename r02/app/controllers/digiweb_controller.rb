4eeb4eb4a173122252567cc92e0f6d53cb795082
dexmon <dexmon@example.com>
2020-02-09 23:52:33 +0900

    Header と UpdateInfoArea をReactで描画する

--- r02/app/controllers/digiweb_controller.rb
+++ r02/app/controllers/digiweb_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class DigiwebController < ApplicationController
-  def show
-    @histories = Digiweb::UpdateHistory.order(id: :DESC)
-  end
+  def show; end
 end
--- r02/client/app/digiweb/App/container.tsx
+++ r02/client/app/digiweb/App/container.tsx
@@ -1,14 +1,3 @@
-import * as React from 'react'
-import { useDispatch } from 'react-redux'
-import * as actions from 'modules/updateHistory/actions'
 import Presenter from './presenter'
 
-export default () => {
-  const dispatch = useDispatch()
-
-  React.useEffect(() => {
-    dispatch(actions.getAll.start())
-  }, [])
-
-  return <Presenter />
-}
+export default Presenter
