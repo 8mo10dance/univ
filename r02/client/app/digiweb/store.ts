4507360625d2ab631cbd37fa2933c5c36f6d14cb
dexmon <dexmon@example.com>
2020-02-09 21:01:06 +0900

    logger を導入する

--- r02/client/app/digiweb/store.ts
+++ r02/client/app/digiweb/store.ts
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import logger from 'redux-logger'
 import rootReducer from 'modules'
 import rootSaga from 'sagas'
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger))
 sagaMiddleware.run(rootSaga)
 
 export default store
