e8bc0fc41b1e114972ece14a75b4ed988fd7188b
Tomoya Sunda <dxm.issatisfied@gmail.com>
2020-02-23 23:49:06 +0900

    MenuList の初期データを Link に移す

---
+++ r02/db/fixtures/links.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+Link.seed(
+  :id,
+  {
+    id: 1,
+    name: 'Leopard-Raws',
+    url: 'http://jp.leopard-raws.org/',
+  },
+  {
+    id: 2,
+    name: 'w3',
+    url: 'http://validator.w3.org/',
+  },
+  {
+    id: 3,
+    name: 'SICP',
+    url:
+      'http://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book.html',
+  },
+  {
+    id: 4,
+    name: 'デジモンサヴァイブ',
+    url: 'https://digimon-sur.bn-ent.net/',
+  },
+  {
+    id: 5,
+    name: '作品データベース',
+    url: 'https://sakuhindb.com/',
+  },
+  {
+    id: 6,
+    name: 'Alice 6',
+    url: 'http://www.konaka.com/index.shtml',
+  }
+)
