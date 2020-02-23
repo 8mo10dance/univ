af4af96403b37f25d714da65176b908260cc547f
Tomoya Sunda <dxm.issatisfied@gmail.com>
2020-02-24 00:13:02 +0900

    links系sagaタスクを実装する

--- r02/client/app/sagas/index.ts
+++ r02/client/app/sagas/index.ts
@@ -2,7 +2,13 @@ import { all } from 'redux-saga/effects'
 import updateHistorySagas from './updateHistory'
 import articleGroupSagas from './articleGroup'
 import listMenuItemSagas from './listMenuItem'
+import linkSagas from './link'
 
 export default function* rootSaga() {
-  yield all([...updateHistorySagas, ...articleGroupSagas, ...listMenuItemSagas])
+  yield all([
+    ...updateHistorySagas,
+    ...articleGroupSagas,
+    ...listMenuItemSagas,
+    ...linkSagas,
+  ])
 }
