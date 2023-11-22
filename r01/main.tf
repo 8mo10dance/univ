174332a7531d4c1f21bad46127e4c1cc4aaf8773
tomoya sunda <dexn.dev@outlook.jp>
2023-11-22 16:40:01 +0900

    (#62) S3 へのファイルアップロード時に Lambda を実行する

--- r01/main.tf
+++ r01/main.tf
@@ -57,6 +57,23 @@ resource "aws_lambda_function" "my_lambda" {
   filename = data.archive_file.lambda_zip.output_path
 }
 
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.my_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.my_bucket.arn
+}
+
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
 
