9b98660cb4593c9f4b70340e09fbba235862031e
tomoya sunda <dexn.dev@outlook.jp>
2023-11-23 19:01:23 +0900

    (#33) 画像アップロード時に署名付きURLを取得する

---
+++ r01/app/javascript/controllers/post_form_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+import qs from 'qs'
+
+export default class extends Controller {
+  async changeImage(event) {
+    const file = event.target.files[0]
+    if (!file) return
+
+    try {
+      const presignedUrl = await getPresignedUrl(file)
+      const response = await uploadFile(presignedUrl, file)
+      if (!response.ok) throw Error('Failed to upload the file')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+async function getPresignedUrl(file) {
+  const params = qs.stringify({ filename: file.name, 'content_type': file.type })
+  const response = await fetch(`/api/v1/presigned_url?${params}`)
+  if (!response.ok) throw Error('Invalid response')
+
+  const data = await response.json()
+  return data.url
+}
+
+function uploadFile(url, file) {
+  const formData = new FormData()
+  formData.append('file', file)
+  const headers = {
+    accept: 'multipart/form-data'
+  }
+  return fetch(url, { method: 'PUT', headers, body: formData })
+}
