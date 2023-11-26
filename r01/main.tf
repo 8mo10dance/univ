fc9b17cc6864a6a148cd2cd5a42d2fec3717d306
tomoya sunda <dexn.dev@outlook.jp>
2023-11-26 15:08:33 +0900

    (#60) archive を使用する

--- r01/main.tf
+++ r01/main.tf
@@ -5,6 +5,10 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 5.26"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = "~> 2.4"
+    }
   }
   backend "local" {}
 }
