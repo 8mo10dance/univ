97b2ac663094a2c6b178e610664a03c37207b5b2
tomoya sunda <dexn.dev@outlook.jp>
2024-02-26 12:13:41 +0900

    GET /tasks を呼び出す

--- js01/src/index.js
+++ js01/src/index.js
@@ -1,1 +1,10 @@
-console.log('hoge')
+call()
+
+async function call() {
+    try {
+        const response = await fetch('http://localhost:3000/api/v1/tasks')
+        console.log(response)
+    } catch (err) {
+        console.log(err)
+    }
+}
